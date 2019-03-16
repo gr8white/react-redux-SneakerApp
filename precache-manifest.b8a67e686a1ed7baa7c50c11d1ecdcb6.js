1cc1937e04098c027341234cbc54bc701200fc00
Derrick White <gr8white@uga.edu>
2019-03-16 10:13:41 -0500

    Updates

---
+++ precache-manifest.b8a67e686a1ed7baa7c50c11d1ecdcb6.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "8677bc69e402c842d44dfef013caea1a",
+    "url": "/static/media/nikeAirForce1.8677bc69.jpg"
+  },
+  {
+    "revision": "aa35c5373ec74dea6406",
+    "url": "/static/css/main.feed718f.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a0474414db54146280ad",
+    "url": "/static/js/2.a0474414.chunk.js"
+  },
+  {
+    "revision": "70bf3237e97e44d30d4228001e1f481e",
+    "url": "/static/media/2w4o22.70bf3237.gif"
+  },
+  {
+    "revision": "63c67d1dca567eeab0c674bf74dad1cd",
+    "url": "/static/media/adidasUndefeated.63c67d1d.jpg"
+  },
+  {
+    "revision": "73e7887bbca7b08d9425f345c7d219c8",
+    "url": "/static/media/chuckTaylorCDG.73e7887b.jpg"
+  },
+  {
+    "revision": "70028f5cdf8249dad52161760214305f",
+    "url": "/static/media/jordan1Bred.70028f5c.jpg"
+  },
+  {
+    "revision": "b5738e74d90e2bdcbb470753a7f9be32",
+    "url": "/static/media/jordan1OffWhite.b5738e74.jpg"
+  },
+  {
+    "revision": "22f8a9bb5e8f869e2e7bac576f039600",
+    "url": "/static/media/jordan1Royal.22f8a9bb.jpg"
+  },
+  {
+    "revision": "cffb38e1f6e6a33e117f38b3f42a0340",
+    "url": "/static/media/jordan3BlackCement.cffb38e1.jpg"
+  },
+  {
+    "revision": "76a2f72ee17195b55cc986cc0bb979c2",
+    "url": "/static/media/jordan3WhiteCement.76a2f72e.jpg"
+  },
+  {
+    "revision": "47446e23c3aa2b421cbeb01e3346b5a5",
+    "url": "/static/media/jordan4WhiteCement.47446e23.jpg"
+  },
+  {
+    "revision": "535c0517b46ac65b94b5f5a6919ee8db",
+    "url": "/static/media/jordan11Bred.535c0517.jpg"
+  },
+  {
+    "revision": "f0edefa77e723f3622d0093fc89329b6",
+    "url": "/static/media/nikeAdapt.f0edefa7.jpg"
+  },
+  {
+    "revision": "aa35c5373ec74dea6406",
+    "url": "/static/js/main.aa35c537.chunk.js"
+  },
+  {
+    "revision": "6ce1d5e1032d486fd7f0cc6c6f708829",
+    "url": "/static/media/nikeAirMax1.6ce1d5e1.jpg"
+  },
+  {
+    "revision": "70d18e03f8e165315d54eee8229a2d9a",
+    "url": "/static/media/nikeEpicReact.70d18e03.jpg"
+  },
+  {
+    "revision": "44a5c4188bce8b45161aa10f07b39297",
+    "url": "/static/media/nikeFlyknitRacer.44a5c418.jpg"
+  },
+  {
+    "revision": "a911a4d3ea57fd6854a880b741fb5b89",
+    "url": "/static/media/nikeFlyknitTrainer.a911a4d3.jpg"
+  },
+  {
+    "revision": "f694fcf0a47bb1f3a14630a98dc22f41",
+    "url": "/static/media/nikeFOGAir.f694fcf0.jpg"
+  },
+  {
+    "revision": "9de59f6ad61178382a4c6f2b58261d45",
+    "url": "/static/media/nikeFOGSkylon.9de59f6a.jpg"
+  },
+  {
+    "revision": "115bfa42258024d866edf5c36776ece7",
+    "url": "/static/media/nikeGalaxyFoamposite.115bfa42.jpg"
+  },
+  {
+    "revision": "5d6a8ccec96843684b17fb64bd6e29f5",
+    "url": "/static/media/nikePresto.5d6a8cce.jpg"
+  },
+  {
+    "revision": "4fa5e57032792ed8a35998fab4bec283",
+    "url": "/static/media/vansFOGEra.4fa5e570.jpg"
+  },
+  {
+    "revision": "2e0041a772961f955a4ef7f470b6e612",
+    "url": "/static/media/vansOldSkool.2e0041a7.jpg"
+  },
+  {
+    "revision": "1abbe917c7c4613ededd6511e1441cd9",
+    "url": "/static/media/vansYachtClub.1abbe917.jpg"
+  },
+  {
+    "revision": "460877d3a881dbdfcc4a9bb565ffca0f",
+    "url": "/static/media/yeezyStatic.460877d3.jpg"
+  },
+  {
+    "revision": "c4e44353b4e84ca1f78c50aa95d8c5a4",
+    "url": "/static/media/nikeDunkPigeon.c4e44353.jpg"
+  },
+  {
+    "revision": "81eca64317695b11d7a5c0ed56d52fc7",
+    "url": "/static/media/adidasSuperstar.81eca643.jpg"
+  },
+  {
+    "revision": "3034ac557382897acd68d7e5322e79c2",
+    "url": "/index.html"
+  }
+];
